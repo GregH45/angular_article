bfbf2aa82660ef31515ff4b0640f760449350b5c
Thomas Le Sciellour <thomas.lesciellour@gmail.com>
2018-03-14 11:35:57 +0100

    Konami function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'app';
+
+  _body: HTMLElement;
+
+  constructor() {
+    this._body = document.body;
+  }
+
+  ngOnInit() {
+    this.hiddenFn();
+  }
+
+  private hiddenFn() {
+    const keys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+    let counter = 0;
+
+    this._body.addEventListener('keyup', (e) => {
+      if (e.keyCode === keys[counter]) {
+        counter++;
+      } else {
+        counter = 0;
+      }
+
+      if (counter === keys.length) {
+        counter = 0;
+        this.fireFn();
+      }
+    });
+  }
+
+  private fireFn() {
+    const dom = document.getElementById('hiddenFunction'),
+        elts = dom.getElementsByTagName('li'),
+        rand = parseInt((elts.length * Math.random()).toString());
+
+    dom.getElementsByClassName('active')[0].classList.remove('active');
+    elts[rand].classList.add('active');
+    dom.classList.add('active');
+
+    setTimeout(() => {
+      dom.classList.remove('active');
+    }, 5000);
+  }
 }
